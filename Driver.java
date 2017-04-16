b92c906ba2ddc376e38d6790b2c347cca9f4e2a1
Leon Joel <leonjoelquainoo@gmail.com>
2017-04-16 02:14:04 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-
 public class Driver implements DriverInterface {
 	RunTime RunTimes = new RunTime();
 	Driver Driver = new Driver();
@@ -23,11 +22,9 @@ public class Driver implements DriverInterface {
 	public RunTime runLinearSearch(int[] listOfNumbers, int target, int numberOfTimes) {
 		RunTime linearSearchRunTime = new RunTime();
 		LinearSearch linSearch = new LinearSearch();
-		for(int x = 0; x < getTargets().length; x++){
-			linearSearchRunTime.resetRunTimes();
-			for (int i = 1; i <= numberOfTimes; i++) {
-				linSearch.search(listOfNumbers, getTargets()[x]);
-			}
+		linearSearchRunTime.resetRunTimes();
+		for (int i = 1; i <= numberOfTimes; i++) {
+			linSearch.search(listOfNumbers, target);
 		}
 		return linearSearchRunTime;
 	}
@@ -36,69 +33,35 @@ public class Driver implements DriverInterface {
 	public RunTime runBinarySearch(int[] listOfNumbers, int target, int numberOfTimes) {
 		BinarySearch binSearch = new BinarySearch();
 		RunTime binarySearchRunTime = new RunTime();
-		for(int x = 0; x < getTargets().length; x++){
-			binarySearchRunTime.resetRunTimes();
-			for (int i = 0; i < numberOfTimes; i++) {
-				binSearch.search(listOfNumbers, getTargets()[x]);
-			}
+		binarySearchRunTime.resetRunTimes();
+		for (int i = 0; i < numberOfTimes; i++) {
+			binSearch.search(listOfNumbers, target);
 		}
 		return binarySearchRunTime;
 	}
 
 	public static void main(String[] args) {
-
 		// Testing LinearSearch Class
-		System.out.println("RUNNING: LinearSearch Class...");
+		System.out.print("RunTimes for Linear Search: ... ");
 		int numberOfTimes = 10;
-		for (int i = 0; i < numberOfTimes; i++) {
-			Driver.runLinearSearch(Driver.getListOfNumbers(), Driver.getTargets()[i], i);
+		for(int x = 0; x < Driver.getTargets().length; x++){
+			for (int i = 0; i < numberOfTimes; i++) {
+				Driver.runLinearSearch(Driver.getListOfNumbers(), Driver.getTargets()[x], i);
+			}
+			System.out.printf(Arrays.toString(RunTimes.getRunTimes())+ "Average = " + RunTimes.getAverageRunTime());
+			System.out.println("Last Run Time: " + RunTimes.getLastRunTime());
 		}
-
-		System.out.print("RunTimes after Running Linear Search: ");
-
-		System.out.println(Arrays.toString(RunTimes.getRunTimes()));
-
-		System.out.println("Last Run Time: " + RunTimes.getLastRunTime());
-
-		System.out.println("Computing Average Run Times:...");
-
-		if (Double.isNaN(RunTimes.getAverageRunTime())) {
-			throw new ArithmeticException("Division by 0");
-		} else {
-			System.out.format("Average = %.2f%n", RunTimes.getAverageRunTime());
-		}
-
-		RunTimes.resetRunTimes();
-		System.out.print("RunTime for Linear Search After Reset: \nAfterReset: ");
-		System.out.print(Arrays.toString(RunTimes.getRunTimes()));
 
 		System.out.println("\n");
 
 		// Testing BinarySearch Class
-		System.out.println("RUNNING: BinarySearch Class...");
-		
-		for (int i = 0; i < numberOfTimes; i++) {
-			Driver.runBinarySearch(Driver.getListOfNumbers(), Driver.getTargets()[i], i);
-		}
-
 		System.out.print("RunTimes for Binary Search: ... ");
-
-		System.out.println(Arrays.toString(RunTimes.getRunTimes()));
-
-		System.out.println("Last Run Time: " + RunTimes.getLastRunTime());
-
-		System.out.println("Computing Average Run Times:...");
-
-		if (Double.isNaN(RunTimes.getAverageRunTime())) {
-			throw new ArithmeticException("Division by 0");
-		} else {
-			System.out.format("Average = %.2f%n", RunTimes.getAverageRunTime());
+		for(int x = 0; x < Driver.getTargets().length; x++){
+			for (int i = 0; i < numberOfTimes; i++) {
+				Driver.runBinarySearch(Driver.getListOfNumbers(), Driver.getTargets()[x], i);
+			}
+			System.out.printf(Arrays.toString(RunTimes.getRunTimes())+ "Average = " + RunTimes.getAverageRunTime());
+			System.out.println("Last Run Time: " + RunTimes.getLastRunTime());
 		}
-
-		RunTimes.resetRunTimes();
-		System.out.println("Resetting all run times... ");
-		
-		System.out.print("RunTime for Binary Search After Reset: ...");
-		System.out.println(Arrays.toString(RunTimes.getRunTimes()));
 	}
 }
