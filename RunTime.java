92c7625e935eb53d2a375b06a53285c8fe8c5da6
Leon Joel <leonjoelquainoo@gmail.com>
2017-06-28 11:46:22 -0400

    Update RunTime.java

--- RunTime.java
+++ RunTime.java
@@ -1,16 +1,12 @@
 public class RunTime  implements RuntimeInterface{
 	long startTime, endTime;
-	static long[] runTimes = new long[10]; //stores the run times
+	static long[] runTimes = new long[10]; 
+	int index = 0; 
+	
 
 	@Override
 	public long getLastRunTime() {
-		int count = 0;
-		for(int i = 0; i < runTimes.length; i++){
-			if(runTimes[i] != 0L){
-				count++;
-			}
-		}
-		return (count >= 1) ? runTimes[count-1] : 0L;
+		return (index >= 1) ? runTimes[index-1] : 0L;
 	}
 
 	@Override
@@ -20,36 +16,30 @@ public class RunTime  implements RuntimeInterface{
 
 	@Override
 	public void addRuntime(long runTime) {
-		for(int i = 0; i < runTimes.length; i++){
-			if(runTimes[i] == 0L){
-				runTimes[i] = runTime;
-				break;
-			}
+		if(index < runTimes.length()){
+			runTimes[index] = runTime;
+			index++;
 		}
 	}
 
 	@Override
 	public void resetRunTimes() {
-		for(int i = 0; i < getRunTimes().length; i++){
-			runTimes[i] = 0L;
-		}
+		Systems.Arrays.fill(runTimes, 0L);
 	}
 
 	@Override
 	public double getAverageRunTime() {
 		double average = 0; long sum = 0;
-		int count = 0;
 		for(int i = 0; i < runTimes.length; i++){
-			if(runTimes[i] != 0L){
-				count++; sum += runTimes[i];
-			}
+			sum += runTimes[i];
 		}
 		try{
-			average = (double) sum / count;
+			return (double) sum / index;
 		}catch (ArithmeticException e){
 			System.out.println("Division by zero");
+			System.out.println(e.getMessage());
 		}
-		return average;
+		return 0.0;
 	}
 
 }
